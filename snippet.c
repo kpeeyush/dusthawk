10caf297fdb664f485ba9ae7302de3998398ff91
Peeyush Kumar <peeyush.ei@gmail.com>
2019-05-27 08:00:51 +0530

    Update snippet.c

--- snippet.c
+++ snippet.c
@@ -1,13 +1,16 @@
+
+osi_EnterCritical();
 for (i = 0; i < len; i++)
 {
 	if (i & 1)
 	{
 		ir_modulation_disable_2();
-		bsp_delay_us(irdata[i]);
+		osi_Busywait(irdata[i]);
 	} 
 	else 
 	{
 		ir_modulation_enable_2();
-		bsp_delay_us(irdata[i]);
+		osi_Busywait(irdata[i]);
 	}
 }
+osi_ExitCritical(0);
