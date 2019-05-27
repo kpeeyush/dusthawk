326dd4e093e33c8bfe86bb8377ccceab24d7b53f
Peeyush Kumar <peeyush.ei@gmail.com>
2019-05-27 06:27:26 +0530

    Update snippet.c

--- snippet.c
+++ snippet.c
@@ -1,29 +1,13 @@
-
-typedef struct {
-	/*Pointer function for LED*/
-	void (*pfnLedNetwork)(int state);
-	void (*pfnLedUser)(int state);
-	
-	/*Pointer function for Switch*/
-	void (*pfnSwitchConfigGet)(int32_t *mode);
-	
-	/*Pointer function for Delay*/
-	void (*pfnDelayMs)(unsigned int ms);
-	void (*pfnReboot)(void);
-	
-	/*Pointer function for NVM*/
-	void (*pfnMemRead)(uint8_t *data, unsigned int ui32Address,
-			unsigned int ui32Count);
-	void (*pfnMemWrite)(uint8_t *data, unsigned int ui32Address,
-			unsigned int ui32Count);
-
-	//Clock
-	int (*pfnGetEpoch)(uint32_t *epoch);
-	int (*pfnSetEpoch)(uint32_t *epoch);
-
-	//Debug
-	long (*pfnDebugPrint)(const char *pcString, ...);
-  .
-  .
-
-} hb_t;
+for (i = 0; i < len; i++)
+{
+	if (i & 1)
+	{
+		ir_modulation_disable_2();
+		bsp_delay_us(irdata[i]);
+	} 
+	else 
+	{
+		ir_modulation_enable_2();
+		bsp_delay_us(irdata[i]);
+	}
+}
